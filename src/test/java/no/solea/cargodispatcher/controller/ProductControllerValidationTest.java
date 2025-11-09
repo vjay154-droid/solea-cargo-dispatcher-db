b4bf0e0d7bf44614986ffeb37d4b3c3632c4feea
SHIVIJ <vijay.shivappa@tryg.no>
2025-11-09 19:31:11 +0100

    fix test case for ProductControllerValidationTest

--- src/test/java/no/solea/cargodispatcher/controller/ProductControllerValidationTest.java
+++ src/test/java/no/solea/cargodispatcher/controller/ProductControllerValidationTest.java
@@ -7,8 +7,8 @@ import no.solea.cargodispatcher.service.ProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
@@ -25,7 +25,7 @@ class ProductControllerValidationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockitoBean   // <-- replacement for @MockBean (deprecated)
+    @MockBean
     private ProductService productService;
 
     @Test
