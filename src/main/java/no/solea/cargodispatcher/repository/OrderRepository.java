71d69c2d8b586d0dc4f04a539a8a544de4a1cec8
SHIVIJ <vijay.shivappa@tryg.no>
2025-11-11 16:18:43 +0100

    Refactor: break down to separate methods

--- src/main/java/no/solea/cargodispatcher/repository/OrderRepository.java
+++ src/main/java/no/solea/cargodispatcher/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package no.solea.cargodispatcher.repository;
 
 import no.solea.cargodispatcher.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
